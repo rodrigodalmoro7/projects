87f09701850520476063b7fb7f8d6041762d3244
unknown <you@example.com>
2020-06-29 21:42:42 -0300

    add modal

--- index.js
+++ index.js
@@ -5,29 +5,49 @@ let scrapsField = document.getElementById("scrapsField");
 
 let scraps = [];
 
+function renderScraps() {
+  scrapsField.innerHTML = "";
+  for (const scrap of scraps) {
+    scrapsField.innerHTML += createScrapCard(scrap.title, scrap.message);
+  }
+}
+
 function addNewScrap() {
   let title = titleInput.value;
   let message = messageInput.value;
 
-  scrapsField.innerHTML = "";
+  if (!messageTitle.value || !messageBody.value) {
+    return alert("Todos os campos devem ser preenchidos!");
+  }
 
+  titleInput.value = "";
+  messageInput.value = "";
   scraps.push({ title, message });
 
-  for (const scrap of scraps) {
-    scrapsField.innerHTML = createScrapCard(scrap.title, scrap.message);
-  }
+  renderScraps();
 }
 
 function createScrapCard(title, message) {
   return `
   <div class="message-cards card text-white bg-dark m-2">
-    <div class="card-header font-weight-bold">${title}</div>
-    <div class="card-body">
-      <p class="card-text">
-        ${message}
-      </p>
-    </div>
-  </div>
+              <div class="card-header font-weight-bold">${title}</div>
+              <div class="card-body">
+                <p class="card-text">
+                  ${message}
+                </p>
+              </div>
+              <div class="w100 d-flex justify-content-end pr-2 pb-2">
+                <button class="btn btn-danger mr-1">Deletar</button>
+                <button
+                  class="btn btn-info"
+                  data-toggle="modal"
+                  data-target="#editModal"
+                >
+                  Editar
+                </button>
+              </div>
+            </div>
+          </div>
   `;
 }
 

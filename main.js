1356524928b0d539e2c0a4d86b1b4f34b208c2d5
unknown <you@example.com>
2020-07-13 19:18:30 -0300

    Alterando para ECMA

--- main.js
+++ main.js
@@ -6,6 +6,56 @@ class TaskList {
     this.scrapsField = document.getElementById("scrapsField");
 
     this.scraps = [];
+    this.registerEvents();
+  }
+
+  registerEvents() {
+    this.addButton.onclick = this.addNewScrap;
+  }
+
+  renderScraps() {
+    this.scrapsField.innerHTML = "";
+
+    for (const scrap of this.scraps) {
+      this.scrapsField.innerHTML += createScrapCard(scrap.title, scrap.message);
+    }
+  }
+
+  addNewScrap() {
+    let title = this.titleInput.nodeValue;
+    let message = this.messageInput.nodeValue;
+
+    this.titleInput.value = "";
+    this.messageInput.value = "";
+
+    this.scraps.push({ title, message });
+
+    this.renderScrap();
+  }
+
+  createScrapCard(title, messade) {
+    return `
+  <div class="message-cards card text-white bg-dark m-2">
+              <div class="card-header font-weight-bold">${title}</div>
+              <div class="card-body">
+                <p class="card-text">
+                  ${message}
+                </p>
+              </div>
+              <div class="w100 d-flex justify-content-end pr-2 pb-2">
+
+             
+                <button class="btn btn-danger mr-1" onclick="deleteCard(${position})">Deletar</button>
+              
+              
+                <button class="btn btn-info"
+                  onclick="openEditModal(${position})"
+                >
+                  Editar
+                </button>
+              </div>
+            </div>
+  `;
   }
 }
 
